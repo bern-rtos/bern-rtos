663af7673c6a9987fa7b009a1fda796914dab647
Stefan Lüthi <stefan.luethi@outlook.com>
2021-10-23 20:54:52 +0200

    Change linked list to heap allocation

--- kernel/src/mem/mod.rs
+++ kernel/src/mem/mod.rs
@@ -1,11 +1,9 @@
 //! Data structures.
 
-pub(crate) mod pool_allocator;
-pub(crate) mod array_pool;
 pub mod boxed;
 pub mod linked_list;
-pub(crate) mod strict_allocator;
-pub(crate) mod allocator;
+pub mod allocator;
+pub mod strict_allocator;
 
 pub use bern_arch::arch::memory_protection::Size;
 pub use bern_arch::{size_from_raw, alignment_from_size};
