4688bfb304d3b5e1cfae53224465b00eb962a611
Stefan Lüthi <stefan.luethi@outlook.com>
2022-02-09 18:22:46 +0100

    Update worker queue to use const pool

--- kernel/src/exec/worker.rs
+++ kernel/src/exec/worker.rs
@@ -1,46 +1,96 @@
 use core::cell::Cell;
+use core::{mem, ptr};
 use core::ptr::NonNull;
 use core::sync::atomic::{AtomicU8, Ordering};
-use core::ops::Deref;
+use core::ops::{Deref, DerefMut};
 use crate::alloc::allocator::AllocError;
+use crate::alloc::const_pool::{ConstBox, Item};
 use crate::exec::process;
 use crate::exec::process::Process;
 use crate::exec::runnable::Priority;
 use crate::exec::thread::Thread;
 use crate::mem::boxed::Box;
-use crate::mem::queue::FiFoQueue;
-use crate::mem::queue::mpsc_const::ConstQueue;
+use crate::mem::queue::mpmc_linked::{Node, Queue};
 use crate::stack::Stack;
-use crate::sync::channel::RefMessage;
 use crate::syscall;
 
 //pub trait WorkTrait: 'static + FnOnce() { }
+pub trait Workable {
+    fn process(&self);
+    fn release(&self);
+}
 
-pub struct ConstWorkqueue<const N: usize> {
+pub struct WorkItem<T> {
+    owner: NonNull<Item<WorkItem<T>>>,
+    trait_node: Node<&'static dyn Workable>,
+    data: T,
+    function: fn(&T),
+}
+
+impl<T> WorkItem<T> {
+
+    fn trait_node(&'static mut self) -> &Node<&dyn Workable> {
+        // todo: remove this lifetime hack
+        let self_ref = unsafe { &mut *(self as *mut _) };
+        self.trait_node = Node::new(self_ref);
+        &self.trait_node
+    }
+}
+
+impl<T> Workable for WorkItem<T> {
+    fn process(&self) {
+        (self.function)(&self.data);
+    }
+
+    fn release(&self) {
+        unsafe {
+            ptr::drop_in_place(self.owner.as_ptr())
+        }
+    }
+}
+
+impl<T> Deref for WorkItem<T> {
+    type Target = T;
+
+    fn deref(&self) -> &Self::Target {
+        &self.data
+    }
+}
+
+impl<T> DerefMut for WorkItem<T> {
+    fn deref_mut(&mut self) -> &mut Self::Target {
+        &mut self.data
+    }
+}
+
+pub struct Workqueue {
     process: &'static Process,
-    work: ConstQueue<RefMessage<fn()>, { N }>,
+    work: Queue<&'static dyn Workable>,
     event_id: Cell<usize>,
     ref_count: AtomicU8,
 }
 
-impl<const N: usize> ConstWorkqueue<{ N }> {
-    pub fn new(context: &process::Context) -> ConstWorkqueueBuilder<{ N }> {
-        ConstWorkqueueBuilder {
+impl Workqueue {
+    pub fn new(context: &process::Context) -> WorkqueueBuilder {
+        WorkqueueBuilder {
             context,
             stack: None,
             priority: Default::default(),
         }
     }
 
-    //pub fn submit<F>(&self, work: F) -> Result<(), AllocError>
-    pub fn submit(&self, work: fn()) -> Result<(), AllocError>
-        //where F: WorkTrait
-    {
-        let boxed_work = Box::try_new_in(work, self.process.allocator())?;
-        let work_ptr = Box::leak(boxed_work).as_ptr();
-        //let entry_ptr = unsafe { &*Box::leak(boxed_work).as_ptr() };
-        self.work.try_push_back(RefMessage::from(work_ptr)).unwrap();
+    pub fn submit<T: 'static>(&self, work: ConstBox<WorkItem<T>>, function: fn(&T)) -> Result<(), AllocError> {
+        let mut item = ConstBox::leak(work);
+        unsafe {
+            (*item.as_mut()).owner = item;
+            (*item.as_mut()).function = function;
+        }
+        let trait_node = unsafe { (*item.as_mut()).trait_node() };
+        unsafe {
+            self.work.push_back(Box::from_raw(NonNull::new_unchecked(trait_node as *const _ as *mut _)));
+        }
 
+        defmt::trace!("Submitting work to queue.");
         syscall::event_fire(self.event_id.get());
         Ok(())
     }
@@ -50,24 +100,26 @@ impl<const N: usize> ConstWorkqueue<{ N }> {
         loop {
             syscall::event_await(self.event_id.get(), u32::MAX).ok();
 
-            while let Ok(work_ref) = self.work.try_pop_front() {
-                unsafe { (*work_ref.into_mut_ptr())() };
+            while let Some(work) = self.work.try_pop_front() {
+                work.process();
+                work.release();
+                Box::leak(work);
             }
         }
     }
 }
 
 // Note(unsafe):
-unsafe impl<const N: usize> Sync for ConstWorkqueue<{ N }> { }
+unsafe impl Sync for Workqueue { }
 
-pub struct ConstWorkqueueBuilder<'a, const N: usize> {
+pub struct WorkqueueBuilder<'a> {
     context: &'a process::Context,
     stack: Option<Stack>,
     /// Woker priority.
     priority: Priority,
 }
 
-impl<'a, const N: usize> ConstWorkqueueBuilder<'a, { N }> {
+impl<'a> WorkqueueBuilder<'a> {
     /// Set worker stack.
     pub fn stack(&mut self, stack: Stack) -> &mut Self {
         self.stack = Some(stack);
@@ -80,8 +132,7 @@ impl<'a, const N: usize> ConstWorkqueueBuilder<'a, { N }> {
         self
     }
 
-    //pub fn build(&mut self) -> &Workqueue {
-    pub fn build(&mut self) -> ConstWorkqueueHandle<{ N }> {
+    pub fn build(&mut self) -> WorkqueueHandle {
         let id = syscall::event_register();
         assert_ne!(id, 0);
 
@@ -91,17 +142,17 @@ impl<'a, const N: usize> ConstWorkqueueBuilder<'a, { N }> {
         };
 
         let worker =
-            Box::try_new_in(ConstWorkqueue {
+            Box::try_new_in(Workqueue {
                 process: self.context.process(),
-                work: ConstQueue::<RefMessage<fn()>, { N }>::new(),
+                work: Queue::new(),
                 event_id: Cell::new(id),
                 ref_count: Default::default(),
             }, self.context.process().allocator());
-        let mut worker = match worker {
+        let worker = match worker {
             Ok(w) => w,
             Err(_) => panic!("No memory left."),
         };
-        let worker_handle = ConstWorkqueueHandle::<{ N }>::from(worker);
+        let worker_handle = WorkqueueHandle::from(worker);
 
         let thread_handle = worker_handle.clone();
         Thread::new(self.context)
@@ -113,35 +164,35 @@ impl<'a, const N: usize> ConstWorkqueueBuilder<'a, { N }> {
     }
 }
 
-pub struct ConstWorkqueueHandle<const N: usize> {
-    workqueue: NonNull<ConstWorkqueue<{ N }>>,
+pub struct WorkqueueHandle {
+    workqueue: NonNull<Workqueue>,
 }
 
-impl<const N: usize> ConstWorkqueueHandle<{ N }> {
-    pub fn new(workqueue: NonNull<ConstWorkqueue<{ N }>>) -> Self {
+impl WorkqueueHandle {
+    pub fn new(workqueue: NonNull<Workqueue>) -> Self {
         unsafe { workqueue.as_ref() }.ref_count.fetch_add(1, Ordering::Relaxed);
-        ConstWorkqueueHandle {
+        WorkqueueHandle {
             workqueue,
         }
     }
 }
 
-impl<const N: usize> Deref for ConstWorkqueueHandle<{ N }> {
-    type Target = ConstWorkqueue<{ N }>;
+impl Deref for WorkqueueHandle {
+    type Target = Workqueue;
 
     fn deref(&self) -> &Self::Target {
         unsafe { &(*self.workqueue.as_ref()) }
     }
 }
 
-impl<const N: usize> From<Box<ConstWorkqueue<{ N }>>> for ConstWorkqueueHandle<{ N }> {
-    fn from(boxed: Box<ConstWorkqueue<{ N }>>) -> Self {
-        ConstWorkqueueHandle::new(Box::leak(boxed))
+impl From<Box<Workqueue>> for WorkqueueHandle {
+    fn from(boxed: Box<Workqueue>) -> Self {
+        WorkqueueHandle::new(Box::leak(boxed))
     }
 }
 
-impl<const N: usize> Clone for ConstWorkqueueHandle<{ N }> {
+impl Clone for WorkqueueHandle {
     fn clone(&self) -> Self {
-        ConstWorkqueueHandle::new(self.workqueue)
+        WorkqueueHandle::new(self.workqueue)
     }
 }
