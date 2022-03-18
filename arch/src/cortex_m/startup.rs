1641004d4d5e8b11a6eda5510c9834d094645e14
Stefan Lüthi <stefan.luethi@outlook.com>
2022-03-18 14:18:32 +0100

    Replace deprecated r0 with assembly

--- arch/src/cortex_m/startup.rs
+++ arch/src/cortex_m/startup.rs
@@ -1,9 +1,18 @@
 //! ARM Cortex-M implementation of [`IStartup`].
 
+use core::arch::asm;
 use crate::IStartup;
 use crate::arch::Arch;
-use r0;
 use crate::startup::Region;
+
+extern "C" {
+    static mut __smkernel: usize;
+    static mut __emkernel: usize;
+    static __sikernel: usize;
+
+    static mut __shkernel: usize;
+    static mut __ehkernel: usize;
+}
 
 // todo: r0 is deprecated, replace with assembly
 
@@ -17,7 +26,42 @@ impl IStartup for Arch {
                 Some(d) => d,
             } as *const u32;
 
-            r0::init_data(start, end, data);
+            asm!(
+                "movs r3, #0
+                  b 1f
+                0:
+                  ldr r4, [r2, r3]
+                  str r4, [r0, r3]
+                  adds r3, r3, #4
+                1:
+                  adds r4, r0, r3
+                  cmp r4, r1
+                  bcc 0b",
+                in("r0") start,
+                in("r1") end,
+                in("r2") data
+            )
+        }
+    }
+
+
+    fn kernel_data() -> Region {
+        unsafe {
+            Region {
+                start: &__smkernel as *const _,
+                end: &__emkernel as *const _,
+                data: Some(&__sikernel as *const _)
+            }
+        }
+    }
+
+    fn kernel_heap() -> Region {
+        unsafe {
+            Region {
+                start: &__shkernel as *const _,
+                end: &__ehkernel as *const _,
+                data: None
+            }
         }
     }
 }
